de699437a792c7a28c584b185575187c357f63c4
llister15 <llister@wonkasoft.com>
2017-09-04 23:37:01 -0700

    Added folder structure

---
+++ wp-member-keeper.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * @link              https://wonkasoft.com
+ * @since             1.0.0
+ * @package           WP_Member_Keeper
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Member Keeper
+ * Plugin URI:        https://wonkasoft.com/wp-member-keeper
+ * Description:       This plugin is to log membership information
+ * Version:           1.0.0
+ * Author:            Wonkasoft
+ * Author URI:        https://wonkasoft.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wp-jam-session
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
