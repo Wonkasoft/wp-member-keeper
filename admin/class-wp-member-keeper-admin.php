50183ff4e7cee28d2be06600c6b45c16124aee74
MrLister <rlister@wonkasoft.com>
2020-08-17 20:53:54 -0700

    fixed update

--- admin/class-wp-member-keeper-admin.php
+++ admin/class-wp-member-keeper-admin.php
@@ -376,7 +376,7 @@ class Wp_Member_Keeper_Admin {
 			$return = array(
 				'msg'  => 'Members info retrived.',
 			);
-			array_push( $return, json_decode( $results ) );
+			json_decode( array_push( $return, $results ), true );
 		else :
 			$return = array(
 				'msg'  => 'No Member found by that ID.',
